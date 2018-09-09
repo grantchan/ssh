097b3376774aaf6d35114779cb7e8e3fe0e986c8
grantchan <chan.grant@gmail.com>
2018-09-08 20:34:47 -0700

    Add DigestFactory for hashing

---
+++ src/main/java/io/github/grantchan/ssh/kex/DigestFactory.java
@@ -0,0 +1,39 @@
+package io.github.grantchan.ssh.kex;
+
+import io.github.grantchan.ssh.common.Factory;
+import io.github.grantchan.ssh.common.NamedObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Set;
+
+public enum DigestFactory implements NamedObject, Factory<MessageDigest> {
+
+  sha1("sha1");
+
+  public static MessageDigest instance = null;
+
+  public static final Set<DigestFactory> values =
+      Collections.unmodifiableSet(EnumSet.allOf(DigestFactory.class));
+
+  private String name;
+
+  DigestFactory(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public MessageDigest create() throws Exception {
+    if (instance == null) {
+      instance = MessageDigest.getInstance(name);
+    }
+    return instance;
+  }
+
+  @Override
+  public String getName() {
+    return this.name;
+  }
+}
