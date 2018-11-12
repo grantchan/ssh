eeb9597db17c1dddecbb624a81848e2169862f16
grantchan <chan.grant@gmail.com>
2018-11-12 10:23:55 -0800

    Deserialize public key file

--- src/main/java/io/github/grantchan/ssh/userauth/PublicKeyEntry.java
+++ src/main/java/io/github/grantchan/ssh/userauth/PublicKeyEntry.java
@@ -2,7 +2,11 @@ package io.github.grantchan.ssh.userauth;
 
 public class PublicKeyEntry {
 
-  private String keyType;
+  private String type;
+  private byte[] data;
 
-
+  public PublicKeyEntry(String type, byte... data) {
+    this.type = type;
+    this.data = data;
+  }
 }
--- src/main/java/io/github/grantchan/ssh/userauth/method/FileBasedPublicKeyAuth.java
+++ src/main/java/io/github/grantchan/ssh/userauth/method/FileBasedPublicKeyAuth.java
@@ -1,9 +1,13 @@
 package io.github.grantchan.ssh.userauth.method;
 
-import java.io.File;
+import io.github.grantchan.ssh.userauth.PublicKeyEntry;
+import io.netty.util.internal.StringUtil;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.security.PublicKey;
-import java.util.Collection;
-import java.util.Objects;
+import java.util.*;
 
 public class FileBasedPublicKeyAuth extends PublicKeyAuth {
 
@@ -18,6 +22,47 @@ public class FileBasedPublicKeyAuth extends PublicKeyAuth {
   private static Collection<PublicKey> deserializeKeys(File authorizedKeysFile) {
     Objects.requireNonNull(authorizedKeysFile);
 
+    try (InputStream is = new FileInputStream(authorizedKeysFile)) {
+      try (Reader rdr = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+        try (BufferedReader brdr = new BufferedReader(rdr)) {
+          List<PublicKeyEntry> entries = null;
+
+          for (String ln = brdr.readLine(); ln != null; ln = brdr.readLine()) {
+            PublicKeyEntry e = parsePublicKeyEntry(ln);
+            if (e == null) {
+              continue;
+            }
+
+            if (entries == null) {
+              entries = new ArrayList<>();
+            }
+            entries.add(e);
+          }
+        }
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     return null;
+  }
+
+  private static PublicKeyEntry parsePublicKeyEntry(String line) {
+    if (StringUtil.isNullOrEmpty(line)) {
+      return null;
+    }
+    if (line.charAt(0) == '#') {
+      return null;
+    }
+
+    String[] i = line.split(" ");
+    if (i.length < 2) {
+      return null;
+    }
+
+    Base64.Decoder decoder = Base64.getDecoder();
+    byte[] data = decoder.decode(i[1]);
+
+    return new PublicKeyEntry(i[0], data);
   }
 }
