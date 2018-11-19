86610f9ba725f6fbd73cf98222e0898f37cd4bcd
grantchan <chan.grant@gmail.com>
2018-11-19 07:49:42 -0800

    Abstract more code from keydecoder derived class to base class

--- src/main/java/io/github/grantchan/ssh/userauth/method/keydecoder/PublicKeyDecoder.java
+++ src/main/java/io/github/grantchan/ssh/userauth/method/keydecoder/PublicKeyDecoder.java
@@ -1,12 +1,21 @@
 package io.github.grantchan.ssh.userauth.method.keydecoder;
 
+import io.github.grantchan.ssh.util.StreamUtil;
+import io.netty.util.internal.StringUtil;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StreamCorruptedException;
 import java.security.GeneralSecurityException;
 import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public interface PublicKeyDecoder<T extends PublicKey> {
+
+  Collection<String> supportKeyTypes();
 
   default T decode(byte[] key) throws IOException, GeneralSecurityException {
     try (InputStream stream = new ByteArrayInputStream(key)) {
@@ -14,5 +23,20 @@ public interface PublicKeyDecoder<T extends PublicKey> {
     }
   }
 
-  T decode(InputStream key) throws IOException, GeneralSecurityException;
+  default T decode(InputStream key) throws IOException, GeneralSecurityException {
+    String type = StreamUtil.readLengthUtf8(key);
+    if (StringUtil.isNullOrEmpty(type)) {
+      throw new StreamCorruptedException("Incomplete key record - key type is missing");
+    }
+
+    Collection<String> types = supportKeyTypes();
+    if (!types.contains(type)) {
+      throw new InvalidKeySpecException("Invalid key type: " + type + ", expected: " +
+          types.stream().collect(Collectors.joining(",")));
+    }
+
+    return decode0(key);
+  }
+
+  T decode0(InputStream key) throws IOException, GeneralSecurityException;
 }
--- src/main/java/io/github/grantchan/ssh/userauth/method/keydecoder/RSAPublicKeyDecoder.java
+++ src/main/java/io/github/grantchan/ssh/userauth/method/keydecoder/RSAPublicKeyDecoder.java
@@ -1,32 +1,34 @@
 package io.github.grantchan.ssh.userauth.method.keydecoder;
 
 import io.github.grantchan.ssh.util.StreamUtil;
-import io.netty.util.internal.StringUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StreamCorruptedException;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
 import java.security.interfaces.RSAPublicKey;
-import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class RSAPublicKeyDecoder implements PublicKeyDecoder<RSAPublicKey> {
 
-  private final static String KEY_TYPE = "ssh-rsa";
+  private static final List<String> supportKeyTypes = Collections.singletonList("ssh-rsa");
+
+  private static final RSAPublicKeyDecoder instance = new RSAPublicKeyDecoder();
+  public static PublicKeyDecoder<?> getInstance() {
+    return instance;
+  }
 
   @Override
-  public RSAPublicKey decode(InputStream key) throws IOException, GeneralSecurityException {
-    String type = StreamUtil.readLengthUtf8(key);
-    if (StringUtil.isNullOrEmpty(type)) {
-      throw new StreamCorruptedException("Incomplete key record - key type is missing");
-    }
+  public Collection<String> supportKeyTypes() {
+    return supportKeyTypes;
+  }
 
-    if (!type.equals(KEY_TYPE)) {
-      throw new InvalidKeySpecException("Invalid key type: " + type + ", expected: " + KEY_TYPE);
-    }
+  @Override
+  public RSAPublicKey decode0(InputStream key) throws IOException, GeneralSecurityException {
 
     BigInteger e = StreamUtil.readMpInt(key);
     BigInteger n = StreamUtil.readMpInt(key);
