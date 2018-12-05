95754413843ee07c5da72f60f38237977891ca0d
grantchan <chan.grant@gmail.com>
2018-12-05 08:28:27 -0800

    Implement DSA signature verification

--- src/main/java/io/github/grantchan/ssh/trans/signature/DSASignature.java
+++ src/main/java/io/github/grantchan/ssh/trans/signature/DSASignature.java
@@ -2,6 +2,7 @@ package io.github.grantchan.ssh.trans.signature;
 
 import com.sun.xml.internal.txw2.IllegalSignatureException;
 import io.github.grantchan.ssh.util.buffer.ByteBufUtil;
+import io.github.grantchan.ssh.util.buffer.ByteUtil;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 
@@ -55,26 +56,77 @@ public class DSASignature extends Signature {
       }
     }
 
-    int i = 0;
-    while (sig[i] == 0 && ((sig[i+1] & 0x80) == 0) && i < BLOB_STRING_LENGTH - 1) {
-      i++;
-    }
-    int rLen = BLOB_STRING_LENGTH - i;  // length of R
-//
-//    byte[] r = new byte[rLen];
-//    r[0] = 0x02;
-//
-//    System.arraycopy(sig, i, r, 1, rLen - 1);
-//
-//    i = BLOB_STRING_LENGTH;
-//    while (sig[i] == 0 && i < (BLOB_STRING_LENGTH << 1)) {
-//      i++;
-//    }
-//    int sLen = (BLOB_STRING_LENGTH << 1) - i + 1;
-//    byte[] s = new byte[sLen];
-//    s[0] = 0;
-//    System.arraycopy(sig, i, s, 1, sLen - 1);
+    byte[] r = encode(sig, 0, BLOB_STRING_LENGTH);
+    byte[] s = encode(sig, BLOB_STRING_LENGTH, BLOB_STRING_LENGTH);
+    sig = encode(r, s);
 
     return Objects.requireNonNull(instance).verify(sig);
+  }
+
+  /**
+   * The whole buffer is formatted as:<br/>
+   * 0x02 [actual length of signature] 0x00 [signature trimmed zero in front]
+   */
+  private byte[] encode(byte[] data, int off, int len) {
+    while (len > 0 && data[off] == 0) {
+      ++off;
+      --len;
+    }
+
+    if (len <= 0) {
+      throw new IllegalSignatureException("Invalid signature length, length: " + len);
+    }
+
+    boolean needPad = (data[off] & 0x80) != 0;
+
+    int totalLen = len;
+    if (needPad) {  // if the signature bytes starts with a negative byte value
+      ++totalLen;   // we need to pad an extra zero byte in front
+    }
+    byte[] lenBytes = encode(totalLen);
+
+    byte[] buf = new byte[Byte.BYTES + lenBytes.length + totalLen];
+    int cnt = 0;
+    buf[cnt++] = 0x02;
+    System.arraycopy(lenBytes, 0, buf, cnt, lenBytes.length);
+    cnt += lenBytes.length;
+    if (needPad) {
+      buf[cnt++] = 0; // the exta zero padding mentioned above
+    }
+    System.arraycopy(data, off, buf, cnt, len);
+
+    return buf;
+  }
+
+  private byte[] encode(int i) {
+    if (i < 128) {
+      return new byte[]{(byte) i};
+    }
+
+    byte[] nbo = ByteUtil.htonl(i);
+    int n = 0;
+    while (nbo[n] == 0) {
+      ++n;
+    }
+    byte[] buf = new byte[nbo.length - n];
+    System.arraycopy(nbo, n, buf, 0, nbo.length - n);
+
+    return buf;
+  }
+
+  private byte[] encode(byte[] r, byte[] s) {
+    int len = r.length + s.length;
+    byte[] lenBytes = encode(len);
+
+    byte[] buf = new byte[Byte.BYTES + lenBytes.length + len];
+    int cnt = 0;
+    buf[cnt++] = 0x30;
+    System.arraycopy(lenBytes, 0, buf, cnt, lenBytes.length);
+    cnt += lenBytes.length;
+    System.arraycopy(r, 0, buf, cnt, r.length);
+    cnt += r.length;
+    System.arraycopy(s, 0, buf, cnt, s.length);
+
+    return buf;
   }
 }
