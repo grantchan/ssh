c65ee04a75659af7f8d7261702686ea6b2e38619
grantchan <chan.grant@gmail.com>
2020-01-04 00:11:18 +0800

    Improve the performance of SshMessage.from() by caching inverted message data in a lazy supplier

--- src/main/java/io/github/grantchan/sshengine/arch/SshMessage.java
+++ src/main/java/io/github/grantchan/sshengine/arch/SshMessage.java
@@ -1,56 +1,80 @@
 package io.github.grantchan.sshengine.arch;
 
+import io.github.grantchan.sshengine.util.LazySupplier;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
- * <p>Protocol packets have message numbers in the range 1 to 255. These
- * numbers are allocated as follows: </p>
+ * This class defines the SSH message identifiers.
  *
- * <p>Transport layer protocol:<br>
- * 1 to 19    Transport layer generic (e.g., disconnect, ignore,
- * debug, etc.)<br>
- * 20 to 29   Algorithm negotiation<br>
- * 30 to 49   Key exchange method specific (numbers can be reused<br>
- * for different authentication methods)</p>
+ * <p>Protocol packets have message numbers in the range 1 to 255. These numbers are allocated as
+ * follows: </p>
  *
- * <p>User authentication protocol:<br>
- * 50 to 59   User authentication generic<br>
- * 60 to 79   User authentication method specific (numbers can be
- * reused for different authentication methods)</p>
+ * <ul>
+ *   <li>
+ *     Transport layer protocol:<ul>
+ *       <li>1 to 19   Transport layer generic (e.g., disconnect, ignore, debug, etc.)</li>
+ *       <li>20 to 29  Algorithm negotiation</li>
+ *       <li>30 to 49  Key exchange method specific (numbers can be reused for different
+ *           authentication methods)</li></ul>
+ *   </li>
  *
- * <p>Connection protocol:
- * 80 to 89   Connection protocol generic<br>
- * 90 to 127  Channel related messages</p>
+ *   <li>
+ *     User authentication protocol:<ul>
+ *       <li>50 to 59  User authentication generic</li>
+ *       <li>60 to 79  User authentication method specific (numbers can be reused for different
+ *           authentication methods)</li></ul>
+ *   </li>
  *
- * <p>Reserved for client protocols:<br>
- * 128 to 191 Reserved</p>
+ *   <li>
+ *     Connection protocol:<ul>
+ *       <li>80 to 89  Connection protocol generic</li>
+ *       <li>90 to 127  Channel related messages</li></ul>
+ *   </li>
  *
- * <p>Local extensions:<br>
- * 192 to 255 Local extensions</p>
+ *   <li>
+ *     Reserved for client protocols:<ul>
+ *       <li>128 to 191 Reserved</li></ul>
+ *   </li>
  *
+ *   <li>
+ *     Local extensions:<ul>
+ *       <li>192 to 255 Local extensions</li></ul>
+ *   </li>
+ * </ul>
  * @see <a href="https://tools.ietf.org/html/rfc4250#section-4.1.1">Message Numbers</a>
  */
 public final class SshMessage {
 
-  // Message Numbers
-  // <a href="https://tools.ietf.org/html/rfc4250#section-4.1.2">Initial Assignments</a>
+  /*
+   * Message Numbers
+   *
+   * @see <a href="https://tools.ietf.org/html/rfc4250#section-4.1.2">Initial Assignments</a>
+   */
   public static final byte SSH_MSG_DISCONNECT                = 1;   // [SSH-TRANS]
   public static final byte SSH_MSG_IGNORE                    = 2;   // [SSH-TRANS]
   public static final byte SSH_MSG_UNIMPLEMENTED             = 3;   // [SSH-TRANS]
   public static final byte SSH_MSG_DEBUG                     = 4;   // [SSH-TRANS]
   public static final byte SSH_MSG_SERVICE_REQUEST           = 5;   // [SSH-TRANS]
   public static final byte SSH_MSG_SERVICE_ACCEPT            = 6;   // [SSH-TRANS]
+
   public static final byte SSH_MSG_KEXINIT                   = 20;  // [SSH-TRANS]
   public static final byte SSH_MSG_NEWKEYS                   = 21;  // [SSH-TRANS]
 
+  // User authentication
   public static final byte SSH_MSG_USERAUTH_REQUEST          = 50;  // [SSH-USERAUTH]
   public static final byte SSH_MSG_USERAUTH_FAILURE          = 51;  // [SSH-USERAUTH]
   public static final byte SSH_MSG_USERAUTH_SUCCESS          = 52;  // [SSH-USERAUTH]
   public static final byte SSH_MSG_USERAUTH_BANNER           = 53;  // [SSH-USERAUTH]
 
+  // "public key" method
   public static final byte SSH_MSG_USERAUTH_PK_OK            = 60;  // [SSH-USERAUTH]
 
   public static final byte SSH_MSG_GLOBAL_REQUEST            = 80;  // [SSH-CONNECT]
@@ -72,13 +96,14 @@ public final class SshMessage {
   public static final byte SSH_MSG_KEXDH_INIT  = 30;
   public static final byte SSH_MSG_KEXDH_REPLY = 31;
 
-  // Diffie-Hellman Group Exchange Message Numbers
-  // <a href="https://tools.ietf.org/html/rfc4419#section-5">Summary of Message Numbers</a>
-  /**
+  /*
+   * Diffie-Hellman Group Exchange Message Numbers
+   *
    * SSH_MSG_KEX_DH_GEX_REQUEST_OLD is used for backward compatibility.<br>
-   * Instead of sending "min || n || max", the client only sends "n".<br>In
-   * addition, the hash is calculated using only "n" instead of "min || n
-   * || max".
+   * Instead of sending "min || n || max", the client only sends "n".<br>
+   * In addition, the hash is calculated using only "n" instead of "min || n || max".
+   *
+   * @see <a href="https://tools.ietf.org/html/rfc4419#section-5">Summary of Message Numbers</a>
    */
   public static final byte SSH_MSG_KEX_DH_GEX_REQUEST_OLD = 30;
   public static final byte SSH_MSG_KEX_DH_GEX_GROUP       = 31;
@@ -86,8 +111,10 @@ public final class SshMessage {
   public static final byte SSH_MSG_KEX_DH_GEX_REPLY       = 33;
   public static final byte SSH_MSG_KEX_DH_GEX_REQUEST     = 34;
 
-  // Disconnection Messages Reason Codes and Descriptions
-  // <a href="https://tools.ietf.org/html/rfc4250#section-4.2.2">Initial Assignments</a>
+  /* Disconnection Messages Reason Codes and Descriptions
+   *
+   * @see <a href="https://tools.ietf.org/html/rfc4250#section-4.2.2">Initial Assignments</a>
+   */
   public static final int SSH_DISCONNECT_HOST_NOT_ALLOWED_TO_CONNECT    = 1;
   public static final int SSH_DISCONNECT_PROTOCOL_ERROR                 = 2;
   public static final int SSH_DISCONNECT_KEY_EXCHANGE_FAILED            = 3;
@@ -104,10 +131,60 @@ public final class SshMessage {
   public static final int SSH_DISCONNECT_NO_MORE_AUTH_METHODS_AVAILABLE = 14;
   public static final int SSH_DISCONNECT_ILLEGAL_USER_NAME              = 15;
 
-  private static boolean isNumberic(Class<?> clazz) {
+  /**
+   * Reflect current class to invert all public, final, static and numeric fields(variables) that
+   * satisfies the eligible condition.
+   *
+   * @param eligible The {@link Predicate} to customize the scan for specific type of field.
+   * @return         A {@link Map} stores the inverted field content.
+   */
+  private static Map<Integer, List<String>> invertFields(Predicate<? super Field> eligible) {
+    return Arrays.stream(SshMessage.class.getFields())
+                 .filter(f -> {
+                   if (!eligible.test(f)) {
+                     return false;
+                   }
+
+                   int mod = f.getModifiers();
+                   if (!Modifier.isPublic(mod) ||
+                       !Modifier.isFinal(mod) ||
+                       !Modifier.isStatic(mod)) {
+                     return false;
+                   }
+
+                   return isNumeric(f.getType());
+                 })
+                 .collect(Collectors.groupingBy(f -> {
+                   try {
+                     return f.getInt(null);
+                   } catch (IllegalAccessException e) {
+                     return -1;
+                   }
+                 }, TreeMap::new, Collectors.mapping(Field::getName, Collectors.toList())));
+  }
+
+  private static final LazySupplier<Map<Integer, List<String>>> MESSAGE_MAP =
+      new LazySupplier<Map<Integer, List<String>>>() {
+        @Override
+        protected Map<Integer, List<String>> initialize() {
+          return invertFields(f -> f.getName().startsWith("SSH_MSG_"));
+        }
+      };
+
+  private static final LazySupplier<Map<Integer, List<String>>> DISCONNECT_REASON_MAP =
+      new LazySupplier<Map<Integer, List<String>>>() {
+        @Override
+        protected Map<Integer, List<String>> initialize() {
+          return invertFields(f -> f.getName().startsWith("SSH_DISCONNECT_"));
+        }
+      };
+
+  private static boolean isNumeric(Class<?> clazz) {
     if (clazz == null) {
       return false;
-    } else if (Number.class.isAssignableFrom(clazz)) {
+    }
+
+    if (Number.class.isAssignableFrom(clazz)) {
       return true;
     }
 
@@ -115,62 +192,28 @@ public final class SshMessage {
         Float.TYPE, Double.TYPE).indexOf(clazz) >= 0;
   }
 
-  private static Integer toInteger(Number num) {
-    if (num == null) {
-      return null;
-    } else if (num instanceof Integer) {
-      return (Integer) num;
-    }
-
-    return num.intValue();
-  }
-
-  private static String getName(int cmd, Predicate<? super Field> filter) {
-    for (Field f : SshMessage.class.getFields()) {
-      String name = f.getName();
-      if (!filter.test(f)) {
-        continue;
-      }
-
-      int mod = f.getModifiers();
-      if (!Modifier.isPublic(mod) || !Modifier.isFinal(mod) || !Modifier.isStatic(mod)) {
-        continue;
-      }
-
-      Class<?> type = f.getType();
-      if (!isNumberic(type)) {
-        continue;
-      }
-
-      Number val;
-      try {
-        val = (Number) f.get(null);
-      } catch (IllegalAccessException e) {
-        continue;
-      }
-
-      if (toInteger(val).equals(cmd)) {
-        return name;
-      }
-    }
-    return Integer.valueOf(cmd).toString();
-  }
-
   /**
    * Gets the message string from a command value
+   *
    * @param cmd  the command value
    * @return     the message string
    */
   public static String from(int cmd) {
-    return getName(cmd, f -> f.getName().startsWith("SSH_MSG_"));
+    List<String> names = MESSAGE_MAP.get().get(cmd);
+    if (names != null && names.size() == 1) {
+      return names.get(0);
+    }
+
+    return Integer.valueOf(cmd).toString();
   }
 
   /**
    * Gets the disconnect reason from a code value
+   *
    * @param code  the reason code
    * @return      the reason string
    */
   public static String disconnectReason(int code) {
-    return getName(code, f -> f.getName().startsWith("SSH_DISCONNECT"));
+    return DISCONNECT_REASON_MAP.get().get(code).get(0);
   }
 }
--- src/test/java/io/github/grantchan/sshengine/common/SshMessageTest.java
+++ src/test/java/io/github/grantchan/sshengine/common/SshMessageTest.java
@@ -10,36 +10,85 @@ import static org.junit.Assert.assertEquals;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SshMessageTest {
 
+  /**
+   * Tests getting SSH message string from its ID by calling {@link SshMessage#from(int)}, the
+   * following messages are not included:
+   *    SSH_MSG_KEXDH_INIT
+   *    SSH_MSG_KEXDH_REPLY
+   *    SSH_MSG_KEX_DH_GEX_REQUEST_OLD
+   *    SSH_MSG_KEX_DH_GEX_GROUP
+   * since their IDs are ambiguous.
+   *
+   * @see #whenGetNameFromDubiousMessage_shouldReturnID()
+   */
   @Test
-  public void testMessageName() {
+  public void whenGetNameFromUniqueMessage_shouldReturnNameString() {
     assertEquals("SSH_MSG_DISCONNECT", SshMessage.from(SshMessage.SSH_MSG_DISCONNECT));
+    assertEquals("SSH_MSG_IGNORE", SshMessage.from(SshMessage.SSH_MSG_IGNORE));
+    assertEquals("SSH_MSG_UNIMPLEMENTED", SshMessage.from(SshMessage.SSH_MSG_UNIMPLEMENTED));
+    assertEquals("SSH_MSG_DEBUG", SshMessage.from(SshMessage.SSH_MSG_DEBUG));
     assertEquals("SSH_MSG_SERVICE_REQUEST", SshMessage.from(SshMessage.SSH_MSG_SERVICE_REQUEST));
     assertEquals("SSH_MSG_SERVICE_ACCEPT", SshMessage.from(SshMessage.SSH_MSG_SERVICE_ACCEPT));
     assertEquals("SSH_MSG_KEXINIT", SshMessage.from(SshMessage.SSH_MSG_KEXINIT));
     assertEquals("SSH_MSG_NEWKEYS", SshMessage.from(SshMessage.SSH_MSG_NEWKEYS));
-    assertEquals("SSH_MSG_KEX_DH_GEX_REQUEST_OLD", SshMessage.from(SshMessage.SSH_MSG_KEX_DH_GEX_REQUEST_OLD));
-    assertEquals("SSH_MSG_KEX_DH_GEX_GROUP", SshMessage.from(SshMessage.SSH_MSG_KEX_DH_GEX_GROUP));
+    assertEquals("SSH_MSG_USERAUTH_REQUEST", SshMessage.from(SshMessage.SSH_MSG_USERAUTH_REQUEST));
+    assertEquals("SSH_MSG_USERAUTH_FAILURE", SshMessage.from(SshMessage.SSH_MSG_USERAUTH_FAILURE));
+    assertEquals("SSH_MSG_USERAUTH_SUCCESS", SshMessage.from(SshMessage.SSH_MSG_USERAUTH_SUCCESS));
+    assertEquals("SSH_MSG_USERAUTH_BANNER", SshMessage.from(SshMessage.SSH_MSG_USERAUTH_BANNER));
+    assertEquals("SSH_MSG_USERAUTH_PK_OK", SshMessage.from(SshMessage.SSH_MSG_USERAUTH_PK_OK));
+    assertEquals("SSH_MSG_GLOBAL_REQUEST", SshMessage.from(SshMessage.SSH_MSG_GLOBAL_REQUEST));
+    assertEquals("SSH_MSG_REQUEST_SUCCESS", SshMessage.from(SshMessage.SSH_MSG_REQUEST_SUCCESS));
+    assertEquals("SSH_MSG_REQUEST_FAILURE", SshMessage.from(SshMessage.SSH_MSG_REQUEST_FAILURE));
+    assertEquals("SSH_MSG_CHANNEL_OPEN", SshMessage.from(SshMessage.SSH_MSG_CHANNEL_OPEN));
+    assertEquals("SSH_MSG_CHANNEL_OPEN_CONFIRMATION", SshMessage.from(SshMessage.SSH_MSG_CHANNEL_OPEN_CONFIRMATION));
+    assertEquals("SSH_MSG_CHANNEL_OPEN_FAILURE", SshMessage.from(SshMessage.SSH_MSG_CHANNEL_OPEN_FAILURE));
+    assertEquals("SSH_MSG_CHANNEL_WINDOW_ADJUST", SshMessage.from(SshMessage.SSH_MSG_CHANNEL_WINDOW_ADJUST));
+    assertEquals("SSH_MSG_CHANNEL_DATA", SshMessage.from(SshMessage.SSH_MSG_CHANNEL_DATA));
+    assertEquals("SSH_MSG_CHANNEL_EXTENDED_DATA", SshMessage.from(SshMessage.SSH_MSG_CHANNEL_EXTENDED_DATA));
+    assertEquals("SSH_MSG_CHANNEL_EOF", SshMessage.from(SshMessage.SSH_MSG_CHANNEL_EOF));
+    assertEquals("SSH_MSG_CHANNEL_CLOSE", SshMessage.from(SshMessage.SSH_MSG_CHANNEL_CLOSE));
+    assertEquals("SSH_MSG_CHANNEL_REQUEST", SshMessage.from(SshMessage.SSH_MSG_CHANNEL_REQUEST));
+    assertEquals("SSH_MSG_CHANNEL_SUCCESS", SshMessage.from(SshMessage.SSH_MSG_CHANNEL_SUCCESS));
+    assertEquals("SSH_MSG_CHANNEL_FAILURE", SshMessage.from(SshMessage.SSH_MSG_CHANNEL_FAILURE));
+
     assertEquals("SSH_MSG_KEX_DH_GEX_INIT", SshMessage.from(SshMessage.SSH_MSG_KEX_DH_GEX_INIT));
     assertEquals("SSH_MSG_KEX_DH_GEX_REPLY", SshMessage.from(SshMessage.SSH_MSG_KEX_DH_GEX_REPLY));
     assertEquals("SSH_MSG_KEX_DH_GEX_REQUEST", SshMessage.from(SshMessage.SSH_MSG_KEX_DH_GEX_REQUEST));
   }
 
+  /**
+   * Tests getting ambiguous SSH message string, of which its ID is not unique - represents more
+   * than two SSH messages, by calling {@link SshMessage#from(int)}
+   *
+   * @see #whenGetNameFromUniqueMessage_shouldReturnNameString()
+   */
+  @Test
+  public void whenGetNameFromDubiousMessage_shouldReturnID() {
+    assertEquals("30", SshMessage.from(SshMessage.SSH_MSG_KEXDH_INIT));
+    assertEquals("31", SshMessage.from(SshMessage.SSH_MSG_KEXDH_REPLY));
+    assertEquals("30", SshMessage.from(SshMessage.SSH_MSG_KEX_DH_GEX_REQUEST_OLD));
+    assertEquals("31", SshMessage.from(SshMessage.SSH_MSG_KEX_DH_GEX_GROUP));
+  }
+
+  /**
+   * Tests getting SSH disconnect reason string from its ID by calling {@link SshMessage#disconnectReason(int)}
+   */
   @Test
   public void testDisconnectReason() {
-    assertEquals("SSH_DISCONNECT_HOST_NOT_ALLOWED_TO_CONNECT", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_HOST_NOT_ALLOWED_TO_CONNECT   ));
-    assertEquals("SSH_DISCONNECT_PROTOCOL_ERROR", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_PROTOCOL_ERROR                ));
-    assertEquals("SSH_DISCONNECT_KEY_EXCHANGE_FAILED", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_KEY_EXCHANGE_FAILED           ));
-    assertEquals("SSH_DISCONNECT_RESERVED", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_RESERVED                      ));
-    assertEquals("SSH_DISCONNECT_MAC_ERROR", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_MAC_ERROR                     ));
-    assertEquals("SSH_DISCONNECT_COMPRESSION_ERROR", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_COMPRESSION_ERROR             ));
-    assertEquals("SSH_DISCONNECT_SERVICE_NOT_AVAILABLE", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_SERVICE_NOT_AVAILABLE         ));
+    assertEquals("SSH_DISCONNECT_HOST_NOT_ALLOWED_TO_CONNECT", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_HOST_NOT_ALLOWED_TO_CONNECT));
+    assertEquals("SSH_DISCONNECT_PROTOCOL_ERROR", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_PROTOCOL_ERROR));
+    assertEquals("SSH_DISCONNECT_KEY_EXCHANGE_FAILED", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_KEY_EXCHANGE_FAILED));
+    assertEquals("SSH_DISCONNECT_RESERVED", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_RESERVED));
+    assertEquals("SSH_DISCONNECT_MAC_ERROR", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_MAC_ERROR));
+    assertEquals("SSH_DISCONNECT_COMPRESSION_ERROR", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_COMPRESSION_ERROR));
+    assertEquals("SSH_DISCONNECT_SERVICE_NOT_AVAILABLE", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_SERVICE_NOT_AVAILABLE));
     assertEquals("SSH_DISCONNECT_PROTOCOL_VERSION_NOT_SUPPORTED", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_PROTOCOL_VERSION_NOT_SUPPORTED));
-    assertEquals("SSH_DISCONNECT_HOST_KEY_NOT_VERIFIABLE", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_HOST_KEY_NOT_VERIFIABLE       ));
-    assertEquals("SSH_DISCONNECT_CONNECTION_LOST", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_CONNECTION_LOST               ));
-    assertEquals("SSH_DISCONNECT_BY_APPLICATION", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_BY_APPLICATION                ));
-    assertEquals("SSH_DISCONNECT_TOO_MANY_CONNECTIONS", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_TOO_MANY_CONNECTIONS          ));
-    assertEquals("SSH_DISCONNECT_AUTH_CANCELLED_BY_USER", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_AUTH_CANCELLED_BY_USER        ));
+    assertEquals("SSH_DISCONNECT_HOST_KEY_NOT_VERIFIABLE", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_HOST_KEY_NOT_VERIFIABLE));
+    assertEquals("SSH_DISCONNECT_CONNECTION_LOST", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_CONNECTION_LOST));
+    assertEquals("SSH_DISCONNECT_BY_APPLICATION", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_BY_APPLICATION));
+    assertEquals("SSH_DISCONNECT_TOO_MANY_CONNECTIONS", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_TOO_MANY_CONNECTIONS));
+    assertEquals("SSH_DISCONNECT_AUTH_CANCELLED_BY_USER", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_AUTH_CANCELLED_BY_USER));
     assertEquals("SSH_DISCONNECT_NO_MORE_AUTH_METHODS_AVAILABLE", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_NO_MORE_AUTH_METHODS_AVAILABLE));
-    assertEquals("SSH_DISCONNECT_ILLEGAL_USER_NAME", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_ILLEGAL_USER_NAME             ));
+    assertEquals("SSH_DISCONNECT_ILLEGAL_USER_NAME", SshMessage.disconnectReason(SshMessage.SSH_DISCONNECT_ILLEGAL_USER_NAME));
   }
 }
