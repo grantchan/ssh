73b0ce0f793d77d21a8d55faa1809615c523dcf1
grantchan <chan.grant@gmail.com>
2021-01-06 00:21:20 +0800

    Replace expected exception rule with assertThrows() in PacketDecoderTest

--- src/test/java/io/github/grantchan/sshengine/common/transport/handler/PacketDecoderTest.java
+++ src/test/java/io/github/grantchan/sshengine/common/transport/handler/PacketDecoderTest.java
@@ -8,29 +8,21 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import org.junit.FixMethodOrder;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runners.MethodSorters;
 import org.mockito.Mockito;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PacketDecoderTest {
 
-  @Rule
-  public ExpectedException thrown = ExpectedException.none();
-
   @Test
-  public void whenPacketSizeIsTooSmall_shouldThrowProtocolError() throws Exception {
-    thrown.expect(SshException.class);
-
-    thrown.expectMessage("Invalid packet length: 0");
-    thrown.expect(hasProperty("reason"));
-    thrown.expect(hasProperty("reason", is(SshMessage.SSH_DISCONNECT_PROTOCOL_ERROR)));
-
+  public void whenPacketSizeIsTooSmall_shouldThrowProtocolError() {
     AbstractSession session = Mockito.mock(ServerSession.class);
     Mockito.when(session.getInCipherBlkSize()).thenReturn(8);
     Mockito.when(session.createBuffer()).thenReturn(Unpooled.buffer());
@@ -43,6 +35,9 @@ public class PacketDecoderTest {
     PacketDecoder decoder = new PacketDecoder(session);
     decoder.handlerAdded(ctx);
 
-    decoder.channelRead(ctx, packet);
+    SshException e = assertThrows(SshException.class, () -> decoder.channelRead(ctx, packet));
+
+    assertEquals(e.getMessage(), "Invalid packet length: 0");
+    assertThat(e, hasProperty("reason", is(SshMessage.SSH_DISCONNECT_PROTOCOL_ERROR)));
   }
 }
