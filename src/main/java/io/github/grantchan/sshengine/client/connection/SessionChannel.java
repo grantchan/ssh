4ec3eeb45c1cd9e54ce26fcb674b6307004d4ddd
grantchan <chan.grant@gmail.com>
2021-03-05 12:32:15 +0800

    Replace lock with CompletableFuture to wait for event happens in client channel

--- src/main/java/io/github/grantchan/sshengine/client/connection/SessionChannel.java
+++ src/main/java/io/github/grantchan/sshengine/client/connection/SessionChannel.java
@@ -5,10 +5,7 @@ import io.github.grantchan.sshengine.common.connection.Window;
 import io.github.grantchan.sshengine.util.DaemonThreadFactory;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class SessionChannel extends AbstractClientChannel {
 
@@ -17,9 +14,10 @@ public class SessionChannel extends AbstractClientChannel {
   private static final ExecutorService drainPool =
       Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE, new DaemonThreadFactory());
 
-  protected final Object lock = new Object();
-
   private Future<?> drainer;
+
+  private static final ScheduledExecutorService delayer =
+      new ScheduledThreadPoolExecutor(1, new DaemonThreadFactory());
 
   public SessionChannel(ClientSession session) {
     super(session);
@@ -31,7 +29,7 @@ public class SessionChannel extends AbstractClientChannel {
   }
 
   @Override
-  protected void doOpen() throws IOException {
+  protected void doOpen() {
     drainer = drainPool.submit(() -> {
       Window remoteWnd = getRemoteWindow();
       int pkgSize = remoteWnd.getPacketSize();
@@ -47,18 +45,14 @@ public class SessionChannel extends AbstractClientChannel {
             chOut.write(buf, 0, n);
           }
         } catch (IOException e) {
-          e.printStackTrace();
+          logger.warn("Error happends while reading data from input stream - {}", e.getMessage());
         }
       }
     });
   }
 
   @Override
-  protected void doClose() throws IOException {
-    synchronized (lock) {
-      lock.notifyAll();
-    }
-
+  protected void doClose() {
     if (!drainer.isDone() || !drainer.isCancelled()) {
       drainer.cancel(true);
     }
@@ -66,16 +60,53 @@ public class SessionChannel extends AbstractClientChannel {
 
   @Override
   public void waitFor(State state, long timeout, TimeUnit unit) {
-    synchronized (lock) {
-      try {
-        if (timeout <= 0) {
-          lock.wait();
-        } else {
-          lock.wait(unit.toMillis(timeout));
-        }
-      } catch (InterruptedException e) {
-        e.printStackTrace();
+    if (state == getState()) {
+      return;
+    }
+
+    // the future event to signal once the channel state is changed to
+    CompletableFuture<State> eventToWait = new CompletableFuture<>();
+
+    // by setting the callback listener, above future event will be notified whenever it's completed
+    // or ended up with exception
+    whenStateChanged((current, cause) -> {
+      if (cause != null) {
+        eventToWait.completeExceptionally(cause);
+      } else {
+        eventToWait.complete(current);
       }
+    });
+
+    // use another future event to indicate the target event - eventToWait is ended
+    CompletableFuture<Boolean> finish = CompletableFuture.supplyAsync(() -> {
+      try {
+        eventToWait.get();
+      } catch (InterruptedException | ExecutionException e) {
+        e.printStackTrace();
+
+        return false;
+      }
+
+      logger.debug("{} State has changed to {}", this, getState());
+
+      return true;
+    });
+
+    CompletableFuture<Boolean> cancel = CompletableFuture.supplyAsync(() -> {
+      delayer.schedule(() -> eventToWait.cancel(true), timeout, unit);
+
+      logger.debug("{} Time out before state is changed, state remains: {}", this, getState());
+
+      return false;
+    });
+
+    try {
+      finish.acceptEither(cancel, b -> eventToWait.cancel(!b)).get();
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+    } finally {
+      // to clean up: whenever what happens, it's necessary to fall back the listener
+      whenStateChanged(null);
     }
   }
 }
