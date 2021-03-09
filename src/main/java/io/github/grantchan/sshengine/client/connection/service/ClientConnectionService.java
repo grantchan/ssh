ad121821880cd89873b72198004258f0e7cdcb0f
grantchan <chan.grant@gmail.com>
2021-03-09 22:23:22 +0800

    Fix message handling issue
      Get corresponding channel inside message handle method, since not every message is required to have related channel, eg.SSH_MSG_GLOBAL_REQUEST

--- src/main/java/io/github/grantchan/sshengine/client/connection/service/ClientConnectionService.java
+++ src/main/java/io/github/grantchan/sshengine/client/connection/service/ClientConnectionService.java
@@ -10,6 +10,8 @@ import io.github.grantchan.sshengine.common.connection.Channel;
 import io.github.grantchan.sshengine.common.transport.handler.SessionHolder;
 import io.netty.buffer.ByteBuf;
 
+import java.io.IOException;
+
 public class ClientConnectionService extends AbstractLogger
                                      implements Service, SessionHolder {
 
@@ -28,6 +30,29 @@ public class ClientConnectionService extends AbstractLogger
   public void handle(int cmd, ByteBuf req) throws Exception {
     logger.debug("{} Handling message - {} ...", session, SshMessage.from(cmd));
 
+    switch (cmd) {
+      case SshMessage.SSH_MSG_CHANNEL_OPEN_CONFIRMATION:
+        channelOpenConfirmation(req);
+        break;
+
+      case SshMessage.SSH_MSG_CHANNEL_OPEN_FAILURE:
+        channelOpenFailure(req);
+        break;
+
+      case SshMessage.SSH_MSG_CHANNEL_DATA:
+        channelData(req);
+        break;
+
+      case SshMessage.SSH_MSG_CHANNEL_EXTENDED_DATA:
+        channelExtendedData(req);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  private void channelOpenConfirmation(ByteBuf req) throws IOException {
     int id = req.readInt();
 
     ClientChannel channel = (ClientChannel) Channel.get(id);
@@ -35,25 +60,41 @@ public class ClientConnectionService extends AbstractLogger
       throw new IllegalStateException("Channel not found - id:" + id);
     }
 
-    switch (cmd) {
-      case SshMessage.SSH_MSG_CHANNEL_OPEN_CONFIRMATION:
-        channel.handleOpenConfirmation(req);
-        break;
-
-      case SshMessage.SSH_MSG_CHANNEL_OPEN_FAILURE:
-        channel.handleOpenFailure(req);
-        break;
-
-      case SshMessage.SSH_MSG_CHANNEL_DATA:
-        channel.handleData(req);
-        break;
-
-      case SshMessage.SSH_MSG_CHANNEL_EXTENDED_DATA:
-        channel.handleExtendedData(req);
-        break;
-
-      default:
-        break;
-    }
+    channel.handleOpenConfirmation(req);
   }
+
+  private void channelOpenFailure(ByteBuf req) {
+    int id = req.readInt();
+
+    ClientChannel channel = (ClientChannel) Channel.get(id);
+    if (channel == null) {
+      throw new IllegalStateException("Channel not found - id:" + id);
+    }
+
+    channel.handleOpenFailure(req);
+  }
+
+  private void channelData(ByteBuf req) {
+    int id = req.readInt();
+
+    ClientChannel channel = (ClientChannel) Channel.get(id);
+    if (channel == null) {
+      throw new IllegalStateException("Channel not found - id:" + id);
+    }
+
+    channel.handleData(req);
+  }
+
+  private void channelExtendedData(ByteBuf req) {
+    int id = req.readInt();
+
+    ClientChannel channel = (ClientChannel) Channel.get(id);
+    if (channel == null) {
+      throw new IllegalStateException("Channel not found - id:" + id);
+    }
+
+    channel.handleExtendedData(req);
+  }
+
+
 }
